a2c352969344f20a9d781bbc4a317feaa1b1ade8
kyleawayan <kyle@awayan.com>
2020-10-05 16:57:47 -0700

    only connect on render

--- components/game.js
+++ components/game.js
@@ -1,23 +1,34 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "../styles/game.module.css";
 
+const io = require("socket.io-client");
+const socket = io("http://localhost:8000");
 
-export default function Game({ children }) {
-  var socket = require("socket.io-client")("http://localhost:8000");
+export default function Game() {
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
     return () => socket.disconnect();
   }, []);
 
+  function setUser(event) {
+    setUsername(event.target.value);
+  }
+
   function putUsername() {
-    socket.emit("event", "hi");
+    console.log(username);
+    socket.emit("event", username);
   }
 
   return (
     <div className={styles.box}>
       <div className={styles.text}>
         <h1>Username</h1>
-        <input className={styles.form}></input>
+        <input
+          className={styles.form}
+          value={username}
+          onChange={setUser}
+        ></input>
         <button onClick={putUsername}>play</button>
       </div>
     </div>
