1cc68d396cb81d1461caecf6e5b9c60c1b3a484b
kyleawayan <kyle@awayan.com>
2020-10-05 15:59:02 -0700

    get something working

--- components/game.js
+++ components/game.js
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import styles from "../styles/game.module.css";
 
+
 export default function Game({ children }) {
+  var socket = require("socket.io-client")("http://localhost:8000");
+
+  useEffect(() => {
+    return () => socket.disconnect();
+  }, []);
+
+  function putUsername() {
+    socket.emit("event", "hi");
+  }
+
   return (
     <div className={styles.box}>
       <div className={styles.text}>
-        <h1>Where is it?</h1>
+        <h1>Username</h1>
         <input className={styles.form}></input>
+        <button onClick={putUsername}>play</button>
       </div>
     </div>
   );
