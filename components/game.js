23d7b60fb0e4bace768a6c26dc5a3d1c63ad0835
kyleawayan <kyle@awayan.com>
2020-10-05 12:35:35 -0700

    add font and input

--- components/game.js
+++ components/game.js
@@ -1,5 +1,12 @@
 import styles from "../styles/game.module.css";
 
 export default function Game({ children }) {
-  return <div className={styles.box}></div>;
+  return (
+    <div className={styles.box}>
+      <div className={styles.text}>
+        <h1>Where is it?</h1>
+        <input className={styles.form}></input>
+      </div>
+    </div>
+  );
 }
