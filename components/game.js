fe1651732653fea17fc95e7c1b4f98477f7eba22
kyleawayan <kyle@awayan.com>
2020-10-06 02:25:30 -0700

    get ready for deployment

--- components/game.js
+++ components/game.js
@@ -12,7 +12,7 @@ import {
 } from "react-google-maps";
 
 const io = require("socket.io-client");
-const socket = io("https://driveon-server.kylan.io");
+const socket = io("https://drive-on-server.herokuapp.com");
 console.log("connecting");
 
 export default function Game() {
