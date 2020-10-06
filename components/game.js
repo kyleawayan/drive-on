5f34ee650a857a5407bb0b577c5909e41212a9ea
kyleawayan <kyle@awayan.com>
2020-10-05 21:32:52 -0700

    make username thing

--- components/game.js
+++ components/game.js
@@ -5,12 +5,15 @@ import { useRouter } from "next/router";
 
 const io = require("socket.io-client");
 const socket = io("http://localhost:8000");
+console.log("connecting");
 
 export default function Game() {
   const router = useRouter();
   const [username, setUsername] = useState("");
   const [hideMakeLobby, setHideMakeLobby] = useState(styles.makelobby);
   const [showLobby, setShowLobby] = useState(styles.hidden);
+  const [players, setPlayers] = useState([]);
+  let playersArr = [players];
 
   useEffect(() => {
     console.log(router.query.id);
@@ -26,17 +29,21 @@ export default function Game() {
     setUsername(event.target.value);
   }
 
+  var id = "";
+
   function putUsername() {
     setHideMakeLobby(styles.hidden);
     setShowLobby(styles.lobby);
-    if (router.query.id !== undefined) {
-        const id = nanoid(5);
-        router.push("/multiplayer", `/multiplayer?id=${id}`, { shallow: true });
-        socket.on("connection", (socket) => {
-          socket.join(id);
-        });
-      }
+    if (router.query.id == undefined) {
+      var id = nanoid(5);
+      router.push("/multiplayer", `/multiplayer?id=${id}`, { shallow: true });
+      socket.emit("room", id);
+    } else {
+      socket.emit("room", router.query.id);
+    }
     socket.emit("event", username);
+    playersArr.push(username);
+    setPlayers(playersArr);
   }
 
   socket.on("event", function (data) {
@@ -57,6 +64,11 @@ export default function Game() {
         </div>
         <div className={showLobby}>
           <h1>loby</h1>
+          <ol>
+            {players.map((players) => (
+              <li>{players}</li>
+            ))}
+          </ol>
         </div>
       </div>
     </div>
