1c1323b11a7b166da9948370fcc5af5e7b82a8cb
kyleawayan <kyle@awayan.com>
2020-10-08 00:41:48 -0700

    restore api

--- pages/api/getrandomstreetview.js
+++ pages/api/getrandomstreetview.js
@@ -2,37 +2,38 @@ const fetch = require("node-fetch");
 const { parseString } = require("xml2js");
 
 export default (req, res) => {
-  res.end(JSON.stringify({ lat: 38.061315, long: -122.147609 }));
-  // var latt;
-  // var longt;
-  //
-  // async function getRandomChords() {
-  //   await fetch("https://api.3geonames.org/?randomland=yes")
-  //     .then((res) => res.text())
-  //     .then((body) => {
-  //       parseString(body, function (err, result) {
-  //         latt = result.geodata.nearest[0].latt[0];
-  //         longt = result.geodata.nearest[0].longt[0];
-  //         checkIfStreetView();
-  //       });
-  //     });
-  //
-  //   console.log(latt, longt);
-  // }
-  // getRandomChords();
-  //
-  // function checkIfStreetView() {
-  //   fetch(
-  //     `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${latt},${longt}&key=AIzaSyA-2SUuUpop6l87nr2xXlqGPZiRMq0Sq3c`    )
-  //     .then((res) => res.text())
-  //     .then((body) => {
-  //       console.log(body.length);
-  //       if (body.length > 4978) {
-  //         res.end(JSON.stringify({ lat: latt, long: longt }));
-  //         return;
-  //       } else {
-  //         getRandomChords();
-  //       }
-  //     });
-  // }
+  // res.end(JSON.stringify({ lat: 38.061315, long: -122.147609 }));
+  var latt;
+  var longt;
+
+  async function getRandomChords() {
+    await fetch("https://api.3geonames.org/?randomland=yes")
+      .then((res) => res.text())
+      .then((body) => {
+        parseString(body, function (err, result) {
+          latt = result.geodata.nearest[0].latt[0];
+          longt = result.geodata.nearest[0].longt[0];
+          checkIfStreetView();
+        });
+      });
+
+    console.log(latt, longt);
+  }
+  getRandomChords();
+
+  function checkIfStreetView() {
+    fetch(
+      `https://maps.googleapis.com/maps/api/streetview?size=400x400&location=${latt},${longt}&key=AIzaSyA-2SUuUpop6l87nr2xXlqGPZiRMq0Sq3c`
+    )
+      .then((res) => res.text())
+      .then((body) => {
+        console.log(body.length);
+        if (body.length > 4978) {
+          res.end(JSON.stringify({ lat: latt, long: longt }));
+          return;
+        } else {
+          getRandomChords();
+        }
+      });
+  }
 };;;;;;
