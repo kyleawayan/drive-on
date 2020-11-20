486a11dffd72b2b5b57a42d60000e15ce6d4890e
Kyle Awayan <56655579+kyleawayan@users.noreply.github.com>
2020-11-19 22:40:34 -0800

    Update game.js

--- components/game.js
+++ components/game.js
@@ -9,7 +9,7 @@ var throttle = require("lodash.throttle");
 
 //https://dev.to/bravemaster619/how-to-prevent-multiple-socket-connections-and-events-in-react-531d
 const io = require("socket.io-client");
- const socket = io("https://driveonserver.kylan.io", {
+ const socket = io("https://driveonserver-kylan.herokuapp.com/", {
    transport: ["websocket"],
  });
 // const socket = io("localhost:8000", {
